4c5afddbe852ef8ac9465ce1d0e3898be856a0f8
Thomas Perez <thomasperez@pursuit.org>
2019-04-11 13:14:43 -0400

    posts-query

--- backend/db/queries/posts_queries.js
+++ backend/db/queries/posts_queries.js
@@ -1,3 +1,63 @@
-// const { db } = require('../index.js');
-//
-// module.exports = { getAllPosts, getSinglePost, getAllCommentsFromPost, createPost, deletePost };
+const { db } = require("../index.js");
+
+const getAllPosts = (req, res, next) => {
+  db.any("SELECT * FROM posts").then(data => {
+    res.status(200).json({
+      status: "Success",
+      data: data,
+      message: "YOU GOT POSTS"
+    });
+  });
+};
+
+const getSinglePost = (req, res, next) => {
+  let postId = parseInt(req.params.id);
+  db.one("SELECT * FROM posts WHERE id=$1", [postId])
+    .then(data => {
+      res.status(200).json({
+        status: "Success",
+        data: data,
+        message: "THATS ONE POST"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
+
+const createPost = (req, res, next) => {
+  db.none(
+    "INSERT INTO posts (user_id, title, pet_type, post_body) VALUES (${user_id}, ${title}, ${pet_id}, ${post_body})",
+    req.body
+  )
+    .then(() => {
+      res.status(200).json({
+        message: "OH THATS A POST"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
+
+const deletePost = (req, res, next) => {
+  let postId = parseInt(req.params.id);
+  db.result("DELETE FROM posts WHERE id=$1", [postId])
+    .then(result => {
+      res.status(200).json({
+        status: "Success",
+        message: "POST ALL GONE",
+        result: result
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
+
+module.exports = {
+  getAllPosts,
+  getSinglePost,
+  createPost,
+  deletePost
+};
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,16 @@
-// const express = require('express');
-// const router = express.Router();
-//
-// const { getAllPosts, getSinglePost, getAllCommentsFromPost, createPost, deletePost } = require('../db/queries/posts_queries.js');
-//
-// router.get('/', getAllPosts);
-// router.get('/:id', getSinglePost);
-// router.get('/comments/:id', getAllCommentsFromPost);
-// router.post('/', createPost);
-// router.delete('/:id', deletePost);
-//
-// module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllPosts,
+  getSinglePost,
+  createPost,
+  deletePost
+} = require("../db/queries/posts_queries.js");
+
+router.get("/", getAllPosts);
+router.post("/", createPost);
+router.get("/:id", getSinglePost);
+router.delete("/:id", deletePost);
+
+module.exports = router;
