003e42f037be71c6a3af823dfc866847af844e43
Thomas Perez <thomasperez@pursuit.org>
2019-04-11 14:46:57 -0400

    post-query

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var shelterRouter = require('./routes/shelters');
-var postsRouter = require('./routes/posts');
-var petsRouter = require('./routes/pets');
-var favoritedRouter = require('./routes/favorited');
-var commentsRouter = require('./routes/comments');
-=======
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -26,7 +11,6 @@ var postsRouter = require("./routes/posts");
 var petsRouter = require("./routes/pets");
 var likesRouter = require("./routes/likes");
 var commentsRouter = require("./routes/comments");
->>>>>>> 0772607c43f9cffc0d7cd76b5b25686bf5e7c13b
 
 var app = express();
 
@@ -40,23 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-<<<<<<< HEAD
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/shelters', sheltersRouter);
-app.use('/posts', postsRouter);
-app.use('/pets', petsRouter);
-app.use('/favorited', favoritedRouter);
-app.use('/comments', commentsRouter);
-=======
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 // app.use('/users', usersRouter);
 // app.use('/shelters', sheltersRouter);
-// app.use('/posts', postsRouter);
+app.use("/posts", postsRouter);
 // app.use('/pets', petsRouter);
 // app.use('/likes', likesRouter);
 app.use("/comments", commentsRouter);
->>>>>>> 0772607c43f9cffc0d7cd76b5b25686bf5e7c13b
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
