dcb6d12e6bb83af97c0af07971433904449cb3f2
Nicolle Loayza <n.loayza@gmail.com>
2019-04-17 19:34:53 -0400

    so mad

--- frontend/src/Navbar/Bar.js
+++ frontend/src/Navbar/Bar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+import { MyContext } from '../provider/MyProvider';
 import { NavLink, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Tabs, Tab, Typography } from '@material-ui/core';
 
+import '../css/navbar.css';
 import '../css/bar.css';
 let logo = require('../assets/logo.png');
 
@@ -40,34 +43,62 @@ class Bar extends React.Component {
     const { value } = this.state;
 
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}  className='navbar'>
 
-          <div className='logoContainer'>
-            <NavLink to={'/'} className="logoLink">
-              <img src={logo} alt="" className="logo"/>
-            </NavLink>
-          </div>
 
-          <div className='fureverHome'>
-           furever home
-          </div>
 
-            <Tab label="BREEDS" style={{ color: '#001049' }} className='iLinks'/>
-            <Tab label="COMMUNITY" style={{ color: '#001049' }} className='iLinks'/>
-          </Tabs>
-        </AppBar>
-        <div className='navlinks'>
-        {value === 2 && <TabContainer>DOGS</TabContainer>}
-        {value === 2 && <TabContainer>CATS</TabContainer>}
-        </div>
-        <div className='navlinks'>
-        {value === 3 && <Link to='/dogCare'><TabContainer>DOG CARE</TabContainer></Link>}
-        {value === 3 && <TabContainer>CAT CARE</TabContainer>}
-        {value === 3 && <TabContainer>SHELETERS & RESCUES</TabContainer>}
-        </div>
-      </div>
+<MyContext.Consumer>
+        {context => {
+          return (
+            <div className={classes.root}>
+              <AppBar position="static">
+                <Tabs
+                  value={value}
+                  onChange={this.handleChange}
+                  className="navbar"
+                >
+                  <div className="logoContainer">
+                    <NavLink to={'/'} className="logoLink">
+                      <img src={logo} alt="" className="logo" />
+                    </NavLink>
+                  </div>
+
+                  <div className="fureverHome">furever home</div>
+                  <Tab
+                    label="BREEDS"
+                    style={{ color: '#001049' }}
+                    className="iLinks"
+                  />
+                  <Tab
+                    label="COMMUNITY"
+                    style={{ color: '#001049' }}
+                    className="iLinks"
+                  />
+                  {context.state.currentUser.username ? (
+                    <div>
+                      <NavLink to={'/profile'}>
+                        {context.state.currentUser.username}
+                      </NavLink>
+                    </div>
+                  ) : (
+                    <div>
+                      <NavLink to={'/login'}>Log in or Register</NavLink>
+                    </div>
+                  )}
+                </Tabs>
+              </AppBar>
+              <div className="navlinks">
+                {value === 2 && <TabContainer>DOGS</TabContainer>}
+                {value === 2 && <TabContainer>CATS</TabContainer>}
+              </div>
+              <div className="navlinks">
+                {value === 3 && <TabContainer>DOG CARE</TabContainer>}
+                {value === 3 && <TabContainer>CAT CARE</TabContainer>}
+                {value === 3 && <TabContainer>SHELTERS & RESCUES</TabContainer>}
+              </div>
+            </div>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
