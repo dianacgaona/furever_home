5a92356174b641b8443568ca708b262491dd923f
DianaG <dianagaona@pursuit.org>
2019-04-17 11:19:35 -0400

    adding provider and changing App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import MyProvider from "./provider/MyProvider";
+import Navbar from "./Navbar/Navbar";
+import Home from "./Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-      <Navbar/>
-
+        <MyProvider>
+          <Navbar />
           <Switch>
-
-            <Route exact path="/" component={Home}/>
-
-            <Route path="/breeds" component={Navbar}/>
-
-            <Route path="/resources" component={Navbar}/>
-
+            <Route exact path="/" component={Home} />
+            <Route path="/breeds" component={Navbar} />
+            <Route path="/community" component={Navbar} />
           </Switch>
-
+        </MyProvider>
       </div>
     );
   }
---
+++ frontend/src/provider/MyProvider.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export const MyContext = React.CreateContext();
+
+class MyProvider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: ""
+    };
+  }
+
+  loginUser = currentUser => {
+    this.setState({
+      currentUser: currentUser
+    });
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          functions: { loginUser: this.loginUser }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+export default MyProvider;
