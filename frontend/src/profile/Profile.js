2a5153093561693c9c8948926887859b5cad3de6
Nicolle Loayza <n.loayza@gmail.com>
2019-04-22 12:17:34 -0400

    homepage carousel

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import FavoritedPets from "./FavoritedPets";
-import AdoptedPets from "./AdoptedPets";
-import UsersPosts from "./UsersPosts";
-import { MyContext } from "../provider/MyProvider";
-=======
+
 import React, { Component } from 'react';
 import FavoritedPets from './FavoritedPets';
 import AdoptedPets from './AdoptedPets';
@@ -12,7 +6,6 @@ import UsersPosts from './UsersPosts';
 import { MyContext } from '../provider/MyProvider';
 import { Paper, Avatar } from '@material-ui/core';
 import '../css/profile.css';
->>>>>>> f1fb1582d860950d37ecac47d22a162259a3edce
 
 class Profile extends Component {
   constructor() {
@@ -22,20 +15,17 @@ class Profile extends Component {
 
   render() {
     return (
-      <MyContext.Consumer>
+      
+
+
+<MyContext.Consumer>
         {context => {
           return (
             <div>
-<<<<<<< HEAD
-              {context.state.currentUser ? (
-                <div>
-                  <h1>{context.state.currentUser.username}</h1>
-=======
               <Paper style={{ padding: '8%' }}>
               {context.state.currentUser ? (
                 <div>
                   <div className='usernameProf'>{context.state.currentUser.username}</div>
->>>>>>> f1fb1582d860950d37ecac47d22a162259a3edce
                   <div>
                   <Avatar alt="Remy Sharp" src={context.state.currentUser.profile_picture} style={{ marginRight: '-11%', marginTop: '-5%', width: '250px', height: '250px' }}
                   />
