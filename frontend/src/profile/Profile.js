96f0e0dfcf1ca6584d54aed6c2c6808f572153bc
DianaG <dianagaona@pursuit.org>
2019-04-21 22:57:32 -0400

    profile changes

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import FavoritedPets from "./FavoritedPets";
-import AdoptedPets from "./AdoptedPets";
-import UsersPosts from "./UsersPosts";
-import { MyContext } from "../provider/MyProvider";
-=======
 import React, { Component } from 'react';
 import FavoritedPets from './FavoritedPets';
 import AdoptedPets from './AdoptedPets';
@@ -12,7 +5,6 @@ import UsersPosts from './UsersPosts';
 import { MyContext } from '../provider/MyProvider';
 import { Paper, Avatar } from '@material-ui/core';
 import '../css/profile.css';
->>>>>>> f1fb1582d860950d37ecac47d22a162259a3edce
 
 class Profile extends Component {
   constructor() {
@@ -22,30 +14,37 @@ class Profile extends Component {
 
   render() {
     return (
-      <MyContext.Consumer>
+      
+
+
+<MyContext.Consumer>
         {context => {
           return (
             <div>
-<<<<<<< HEAD
-              {context.state.currentUser ? (
-                <div>
-                  <h1>{context.state.currentUser.username}</h1>
-=======
               <Paper style={{ padding: '8%' }}>
-              {context.state.currentUser ? (
-                <div>
-                  <div className='usernameProf'>{context.state.currentUser.username}</div>
->>>>>>> f1fb1582d860950d37ecac47d22a162259a3edce
+                {context.state.currentUser ? (
                   <div>
-                  <Avatar alt="Remy Sharp" src={context.state.currentUser.profile_picture} style={{ marginRight: '-11%', marginTop: '-5%', width: '250px', height: '250px' }}
-                  />
+                    <div className="usernameProf">
+                      {context.state.currentUser.username}
+                    </div>
+                    <div>
+                      <Avatar
+                        alt="Remy Sharp"
+                        src={context.state.currentUser.profile_picture}
+                        style={{
+                          marginRight: '-11%',
+                          marginTop: '-5%',
+                          width: '250px',
+                          height: '250px',
+                        }}
+                      />
+                    </div>
+                    <div>{context.state.currentUser.about}</div>
                   </div>
-                  <div>{context.state.currentUser.about}</div>
-                </div>
-              ) : (
-                <div>no user</div>
-              )}
-                </Paper>
+                ) : (
+                  <div>no user</div>
+                )}
+              </Paper>
               <FavoritedPets />
               <UsersPosts />
               <AdoptedPets />
