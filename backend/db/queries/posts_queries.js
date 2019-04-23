248a76c2dd3c1a74c1e871ba33862351e6472341
Andres.C <andrescabrera@pursuit.org>
2019-04-23 13:01:22 -0400

    andres update

--- backend/db/queries/posts_queries.js
+++ backend/db/queries/posts_queries.js
@@ -1,22 +1,40 @@
-const { db } = require('../index.js');
+const { db } = require("../index.js");
 
 const getAllPosts = (req, res, next) => {
-  db.any('SELECT * FROM posts').then(posts => {
+  db.any("SELECT * FROM posts").then(posts => {
     res.status(200).json({
-      status: 'Success',
+      status: "Success",
       posts: posts,
-      message: 'YOU GOT POSTS',
+      message: "YOU GOT POSTS"
     });
   });
 };
 
-const getAllDogPosts = (req, res, next) => {
-  db.any('SELECT * FROM posts WHERE pet_type=$1', ['Dog'])
+const getAllPostsbyUser = (req, res, next) => {
+  let userId = parseInt(req.params.id);
+  db.any(
+    "SELECT posts.id,posts.user_id,posts.title,posts.post_body,posts.pet_type,post_url FROM posts JOIN users ON users.id = posts.user_id WHERE users.id=$1",
+    [userId]
+  )
     .then(post => {
       res.status(200).json({
-        status: 'Success',
+        status: "Success",
         post: post,
-        message: 'GOT ALL DOG POSTS',
+        message: "All Posts by User"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
+
+const getAllDogPosts = (req, res, next) => {
+  db.any("SELECT * FROM posts WHERE pet_type=$1", ["Dog"])
+    .then(post => {
+      res.status(200).json({
+        status: "Success",
+        post: post,
+        message: "GOT ALL DOG POSTS"
       });
     })
     .catch(err => {
@@ -25,12 +43,12 @@ const getAllDogPosts = (req, res, next) => {
 };
 
 const getAllCatPosts = (req, res, next) => {
-  db.any('SELECT * FROM posts WHERE pet_type=$1', ['Cat'])
+  db.any("SELECT * FROM posts WHERE pet_type=$1", ["Cat"])
     .then(post => {
       res.status(200).json({
-        status: 'Success',
+        status: "Success",
         post: post,
-        message: 'GOT ALL CAT POSTS',
+        message: "GOT ALL CAT POSTS"
       });
     })
     .catch(err => {
@@ -40,12 +58,15 @@ const getAllCatPosts = (req, res, next) => {
 
 const getSinglePost = (req, res, next) => {
   let postId = parseInt(req.params.id);
-  db.one('SELECT username,posts.id,user_id,title,post_body,pet_type,post_url FROM posts JOIN users ON users.id = posts.user_id WHERE posts.id=$1', [postId])
+  db.one(
+    "SELECT username,posts.id,user_id,title,post_body,pet_type,post_url FROM posts JOIN users ON users.id = posts.user_id WHERE posts.id=$1",
+    [postId]
+  )
     .then(post => {
       res.status(200).json({
-        status: 'Success',
+        status: "Success",
         post: post,
-        message: 'THATS ONE POST',
+        message: "THATS ONE POST"
       });
     })
     .catch(err => {
@@ -53,16 +74,14 @@ const getSinglePost = (req, res, next) => {
     });
 };
 
-
-
 const createPost = (req, res, next) => {
   db.none(
-    'INSERT INTO posts (user_id, title, pet_type, post_body,post_url) VALUES (${user_id}, ${title}, ${pet_type}, ${post_body},${post_url})',
+    "INSERT INTO posts (user_id, title, pet_type, post_body,post_url) VALUES (${user_id}, ${title}, ${pet_type}, ${post_body},${post_url})",
     req.body
   )
     .then(() => {
       res.status(200).json({
-        message: 'OH THATS A POST',
+        message: "OH THATS A POST"
       });
     })
     .catch(err => {
@@ -72,12 +91,12 @@ const createPost = (req, res, next) => {
 
 const deletePost = (req, res, next) => {
   let postId = parseInt(req.params.id);
-  db.result('DELETE FROM posts WHERE id=$1', [postId])
+  db.result("DELETE FROM posts WHERE id=$1", [postId])
     .then(result => {
       res.status(200).json({
-        status: 'Success',
-        message: 'POST ALL GONE',
-        result: result,
+        status: "Success",
+        message: "POST ALL GONE",
+        result: result
       });
     })
     .catch(err => {
@@ -87,9 +106,10 @@ const deletePost = (req, res, next) => {
 
 module.exports = {
   getAllPosts,
+  getAllPostsbyUser,
   getAllDogPosts,
   getAllCatPosts,
   getSinglePost,
   createPost,
-  deletePost,
+  deletePost
 };
--- frontend/src/profile/UsersPosts.js
+++ frontend/src/profile/UsersPosts.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Paper } from '@material-ui/core';
-import '../css/profile.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Paper } from "@material-ui/core";
+import "../css/profile.css";
+import { MyContext } from "../provider/MyProvider";
+
 
 class UsersPosts extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      singlePost: [],
+      user_Posts: [],
+      currentUser: this.props.currentUser
     };
   }
 
   componentDidMount() {
-    this.getPosts();
-    this.displaySinglePost()
+  this.getPosts();
+    this.displayPosts();
   }
-
-  getPosts = () => {
+  // componentDidUpdate(){
+  // this.displayPosts()
+  // }
+  getPosts = async() => {
+    // const {currentUser} = this.state
+    await
     axios
-      .get('/posts/1')
+      .get(`/posts/byUser/2`)
       .then(res => {
         this.setState({
-          singlePost: res.data.post,
+          user_Posts: res.data.post
         });
         // console.log(res);
       })
@@ -30,26 +37,31 @@ class UsersPosts extends Component {
       });
   };
 
-  displaySinglePost = () => {
-    let { singlePost } = this.state;
+  displayPosts = () => {
+    let posts = this.state.user_Posts.map((post, i) => {
+      return (
+        <div key={i}>
+          <Paper style={{ padding: "2%", marginTop: "1%" }}>
+            <p className="favoritedTitle">{post.title}</p>
+            <img src={post.post_url} alt="imageNotWorking" className="favImg" />
+          </Paper>
+        </div>
+      );
+    });
 
-    return (
-      <div>
-        <Paper style={{ padding: '2%', marginTop: '1%' }}>
-        <p className='favoritedTitle'>{singlePost.title}</p>
-        <img src={singlePost.post_url}alt='imageNotWorking' className='favImg'></img>
-        </Paper>
-      </div>
-    );
+    return <>{posts}</>;
   };
-// needs  to be changed since there will be multiple posts now. Which is why we wont see anything.
-  render() {
-    // console.log(this.state);
-    return (
-      <div>
 
-        {this.displaySinglePost()}
-      </div>
+  render() {
+    console.log(this.state, 'state');
+    console.log(this.props, 'props');
+    return (
+      <MyContext.Consumer>
+        {context => {
+          // this.getPosts(context.state.currentUser.id)
+          return <div> {this.displayPosts()} </div>;
+        }}
+      </MyContext.Consumer>
     );
   }
 }
