dbab48cea0efcc5c9261b8478e3cf4a3ed2ba3cc
DianaG <dianagaona@pursuit.org>
2019-04-25 13:09:11 -0400

    favorited pets are linked to their profile

--- frontend/src/profile/FavoritedPets.js
+++ frontend/src/profile/FavoritedPets.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { MyContext } from "../provider/MyProvider";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { MyContext } from '../provider/MyProvider';
 import { Paper, Avatar } from '@material-ui/core';
 import '../css/profile.css';
+import { Link } from 'react-router-dom';
 
 class FavoritedPets extends Component {
   constructor() {
     super();
     this.state = {
-      userFavs: []
+      userFavs: [],
     };
   }
 
@@ -21,7 +22,7 @@ class FavoritedPets extends Component {
       .get(`/favorited/users/${id}`)
       .then(res => {
         this.setState({
-          userFavs: res.data.favorited
+          userFavs: res.data.favorited,
         });
         console.log(res);
       })
@@ -31,28 +32,37 @@ class FavoritedPets extends Component {
   };
 
   displayFavorites = () => {
-    let favorites = this.state.userFavs.map((favorited, i) => {
+    let favorites = this.state.userFavs;
+    return favorites.map(favorite => {
+      console.log('favorites', favorite);
       return (
-        <img
-          src={`https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/${
-            favorited.pet_id
-          }/1/?bust=1555622095&width=300`}
-          alt=""
-        style={{padding: '1%'}}/>
+        <div key={favorite.id}>
+          <Link to={`/animals/${favorite.pet_id}`}>
+            <img
+              src={`https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/${
+                favorite.pet_id
+              }/1/?bust=1555622095&width=300`}
+              alt=""
+              style={{ padding: '10px' }}
+            />
+          </Link>
+        </div>
       );
     });
-    return <>{favorites}</>;
   };
 
   render() {
     return (
-      <MyContext.Consumer>
+      
+
+
+<MyContext.Consumer>
         {context => {
           return (
             <div>
               <Paper style={{ padding: '2%', marginTop: '1%' }}>
-              <p className="favoritedTitle">Favorited Pets Go Here</p>
-              <div className='favImg'>{this.displayFavorites()}</div>
+                <p className="favoritedTitle">Favorited Pets Go Here</p>
+                <div className="favImg">{this.displayFavorites()}</div>
               </Paper>
             </div>
           );
