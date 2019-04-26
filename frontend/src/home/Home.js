d0f79bce8051983ffb9c5d99c5bfcc7dd56d4701
DianaG <dianagaona@pursuit.org>
2019-04-26 09:36:42 -0400

    css in pet profile

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import '../css/home.css';
-import Wallpaper from './Wallpaper';
-import SearchBar from './SearchBar';
+import React, { Component } from "react";
+import "../css/home.css";
+import Wallpaper from "./Wallpaper";
+// import SearchBar from './SearchBar';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <p className="greeting">WHERE PEOPLE FIND THEIR FURENDS</p>
-        <SearchBar />
+        {/*<SearchBar />*/}
         <div className="wallpaper">
           <Wallpaper />
         </div>
